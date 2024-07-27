4183f436aabb9271c07262d4fa8747fe122df869
Nitin Bhadauria <nitinb@cloudstaff.com>
2024-07-27 20:45:31 +1000

    remove the default tag

--- ecs-task-definition.tf
+++ ecs-task-definition.tf
@@ -64,6 +64,6 @@ resource "aws_ecs_task_definition" "default" {
     replace_triggered_by = [aws_lb_target_group.green]
   }
 
-  tags = merge(var.tags, { "terraform" = "true" }, )
+  tags = var.tags
 
 }
